c6f3ddc13e460d9279cf265f91ed885d419da462
BuildTools <unconfigured@null.spigotmc.org>
2020-03-29 13:29:05 -0400

    Navbar

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// GET home page.
+router.get('/', function(req, res, next) 
+{
+  res.render('index', { title: 'Hermes'});
+});
+
+// GET explore page
+router.get('/explore', function(req, res, next)
+{
+  res.render('explore', { title: 'Hermes'});
+});
+
+// GET service page
+router.get('/service', function(req, res, next)
+{
+  res.render('service', { title: 'Hermes'});
+});
+
+// GET delivery page
+router.get('/delivery', function(req, res, next)
+{
+  res.render('delivery', { title: 'Hermes'});
 });
 
 module.exports = router;
