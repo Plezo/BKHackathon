50318812a14d4314b8c1ff6c32179816c18179d0
BuildTools <unconfigured@null.spigotmc.org>
2020-03-29 13:48:06 -0400

    Updated navbar and organization

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 // GET home page.
 router.get('/', function(req, res, next) 
 {
-  res.render('index', { title: 'Hermes'});
+  res.render('home', { title: 'Hermes'});
 });
 
 // GET explore page
